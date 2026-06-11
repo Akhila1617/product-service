f0659b48977c3a9bd74e501597f9f8242cf9ea15
Akhila Reddy Baddam <akhilareddybaddam@Akhila-Reddys-MacBook-Pro.local>
2026-06-11 15:09:19 -0400

    1E-product-crud-api

---
+++ src/main/java/com/microservices/productservice/controller/ProductController.java
@@ -0,0 +1,33 @@
+package com.microservices.productservice.controller;
+
+import com.microservices.productservice.model.Product;
+import com.microservices.productservice.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productService.addProduct(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
---
+++ src/main/java/com/microservices/productservice/service/ProductService.java
@@ -0,0 +1,29 @@
+package com.microservices.productservice.service;
+
+import com.microservices.productservice.model.Product;
+import com.microservices.productservice.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+}
